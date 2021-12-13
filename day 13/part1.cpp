08058dffeeaace6a0f47caf88e644380e67cd7ba
Luis Galan <luis.sebastian.galan@gmail.com>
2021-12-13 09:58:21 +0100

    day 13

---
+++ day 13/part1.cpp
@@ -0,0 +1,106 @@
+/*
+    ░░▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒██▓▓▒▒▒▒████▓▓▒▒▒▒▒▒░░
+    ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒
+  ▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒Hele▒Danmarks▒pilsner▒▒▒▒▒▒▒░░░░▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒Ø▒▒▒N▒▒▒▒▒T▒▒U▒▒B▒▒O▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒R▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒R▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒G▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒G▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓██████▒██▒▒▒██▒█████▒▒▒█████▒▒█████▒▒▒█████▒▒▒
+██▓▓▒▒██▒▒▒██▒▒▒██▒██▒▒██▒██▒▒▒██▒██▒▒██▒██▒▒▒▒▒▒▒
+██▓▓▒▒██▒▒▒██▒▒▒██▒█████▒▒██▒▒▒██▒█████▒▒██▒▒███▒▒
+██▓▓▒▒██▒▒▒██▒▒▒██▒██▒▒██▒██▒▒▒██▒██▒▒██▒██▒▒▒██▒▒
+██▓▓▒▒██▒▒▒▒█████▒▒█████▒▒▒█████▒▒██▒▒██▒▒█████▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒*▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒*▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒P▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒L▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒I▒▒L▒▒S▒▒N▒▒E▒▒R▒▒▒▒▒▒Ø▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+████▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+████▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██████▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██████████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+  ██████████████████████████████████████████████
+      ██████████████████████████████████████
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+void solve();
+template<class F,class S>ostream&operator<<(ostream&o,const pair<F,S>p){return o<<'('<<p.first<<", "<<p.second<<')';}template<class...V>ostream&operator<<(ostream&o,const tuple<V...>t){o<<'(';apply([&o](auto&&...v){((o<<v<<", "),...);},t);o<<"\b\b)";return o;}template<class C,class T=decay<decltype(*begin(declval<C>()))>,typename enable_if<!is_same<C,string>::value>::type* =nullptr>ostream&operator<<(ostream&o,const C&v){o<<'[';for(auto&x:v)o<<x<<", ";if(!v.empty())o<<"\b\b";return o<<']';}
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define MT(args...) make_tuple(args)
+#define PB(a) push_back(a)
+#define MP(a, b) make_pair(a, b)
+#define FOR(i, n) for(int i = 0; i < (ll)(n); i++)
+#define FORS(i,n) for(int i=1;i<=(n);i++)
+#define FORE(x,a) for(auto &x: a)
+#define FORT(i,a,b) for(int i=(a);i<(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(x) x.begin(),x.end()
+#define F first
+#define S second
+#define VEC(x,y) typedef vector<x> v##y;typedef vector<v##y> vv##y;typedef vector<vv##y> vvv##y;
+#define DEF(x,y) typedef x y;typedef vector<x> v##y;typedef vector<v##y> vv##y;typedef vector<vv##y> vvv##y;
+#define COMMA ,
+VEC(int,i)VEC(char,c)VEC(string,s)DEF(long long,ll)DEF(long double,ld)DEF(complex<ld>,com)DEF(pair<int COMMA int>,ii)DEF(tuple<int COMMA int COMMA int>,iii)
+// #define endl '\n'
+
+const int DX[4] = { 0,  1,  0, -1 };
+const int DY[4] = { 1,  0, -1,  0 };
+const ll INF = LLONG_MAX / 2 + INT_MAX / 2;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << fixed << setprecision(10);
+
+    int num_cases = 1;
+    // cin >> num_cases;
+    while (num_cases--) solve();
+}
+
+const int NUM_POINTS = 983;
+const int NUM_FOLDS = 12;
+
+void solve() {
+    set<ii> points;
+    FOR(i, NUM_POINTS) {
+        int a, b;
+        cin >> a;
+        cin.ignore();
+        cin >> b;
+        points.insert(MP(a, b));
+    }
+
+    set<ii> next;
+    FORE(point, points) {
+        if (point.F >= 655) {
+            next.insert(MP(2 * 655 - point.F, point.S));
+        } else {
+            next.insert(point);
+        }
+    }
+    debug(next);
+    debug(next.size());
+}
+
