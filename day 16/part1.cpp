3540b249761fe188e07f84679650247a7db772d7
Luis Galan <luis.sebastian.galan@gmail.com>
2021-12-16 12:47:32 +0100

    day 16

---
+++ day 16/part1.cpp
@@ -0,0 +1,185 @@
+/*
+    ░░▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒██▓▓▒▒▒▒████▓▓▒▒▒▒▒▒░░
+    ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒
+  ▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒Hele▒Danmarks▒pilsner▒▒▒▒▒▒▒░░░░▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒Ø▒▒▒N▒▒▒▒▒T▒▒U▒▒B▒▒O▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒R▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒R▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒G▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒G▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓██████▒██▒▒▒██▒█████▒▒▒█████▒▒█████▒▒▒█████▒▒▒
+██▓▓▒▒██▒▒▒██▒▒▒██▒██▒▒██▒██▒▒▒██▒██▒▒██▒██▒▒▒▒▒▒▒
+██▓▓▒▒██▒▒▒██▒▒▒██▒█████▒▒██▒▒▒██▒█████▒▒██▒▒███▒▒
+██▓▓▒▒██▒▒▒██▒▒▒██▒██▒▒██▒██▒▒▒██▒██▒▒██▒██▒▒▒██▒▒
+██▓▓▒▒██▒▒▒▒█████▒▒█████▒▒▒█████▒▒██▒▒██▒▒█████▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒*▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒*▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒P▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒L▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒I▒▒L▒▒S▒▒N▒▒E▒▒R▒▒▒▒▒▒Ø▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+████▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+████▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██████▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+██████████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+  ██████████████████████████████████████████████
+      ██████████████████████████████████████
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+void solve();
+template<class F,class S>ostream&operator<<(ostream&o,const pair<F,S>p){return o<<'('<<p.first<<", "<<p.second<<')';}template<class...V>ostream&operator<<(ostream&o,const tuple<V...>t){o<<'(';apply([&o](auto&&...v){((o<<v<<", "),...);},t);o<<"\b\b)";return o;}template<class C,class T=decay<decltype(*begin(declval<C>()))>,typename enable_if<!is_same<C,string>::value>::type* =nullptr>ostream&operator<<(ostream&o,const C&v){o<<'[';for(auto&x:v)o<<x<<", ";if(!v.empty())o<<"\b\b";return o<<']';}
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define MT(args...) make_tuple(args)
+#define PB(a) push_back(a)
+#define MP(a, b) make_pair(a, b)
+#define FOR(i, n) for(int i = 0; i < (ll)(n); i++)
+#define FORS(i,n) for(int i=1;i<=(n);i++)
+#define FORE(x,a) for(auto &x: a)
+#define FORT(i,a,b) for(int i=(a);i<(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(x) x.begin(),x.end()
+#define F first
+#define S second
+#define VEC(x,y) typedef vector<x> v##y;typedef vector<v##y> vv##y;typedef vector<vv##y> vvv##y;
+#define DEF(x,y) typedef x y;typedef vector<x> v##y;typedef vector<v##y> vv##y;typedef vector<vv##y> vvv##y;
+#define COMMA ,
+VEC(int,i)VEC(char,c)VEC(string,s)DEF(long long,ll)DEF(long double,ld)DEF(complex<ld>,com)DEF(pair<int COMMA int>,ii)DEF(tuple<int COMMA int COMMA int>,iii)
+// #define endl '\n'
+
+const int DX[4] = { 0,  1,  0, -1 };
+const int DY[4] = { 1,  0, -1,  0 };
+const ll INF = LLONG_MAX / 2 + INT_MAX / 2;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << fixed << setprecision(10);
+
+    int num_cases = 1;
+    // cin >> num_cases;
+    while (num_cases--) solve();
+}
+
+ll parse_number(int *number, int length) {
+    ll res = 0;
+    FOR(i, length) {
+        res += number[i] << (length - i - 1);
+    }
+    return res;
+}
+
+ll version_number(int *packet) {
+    return parse_number(packet, 3);
+}
+
+ll length_type(int *packet) {
+    return packet[6];
+}
+
+ll packet_type(int *packet) {
+    return parse_number(&packet[3], 3);
+}
+
+ll parse_packet(vi &bits, int &idx);
+
+ll parse_lt0(vi &bits, int &idx) {
+    cerr << "parsing lt0" << endl;
+    int num_bits = parse_number(&bits[idx + 7], 15);
+    // debug(num_bits);
+    ll sum = version_number(&bits[idx]);
+    idx += 22;
+    int start = idx;
+    while (idx - start < num_bits) {
+        sum += parse_packet(bits, idx);
+    }
+    return sum;
+}
+
+ll parse_lt1(vi &bits, int &idx) {
+    cerr << "parsing lt1" << endl;
+    int num_subpackets = parse_number(&bits[idx + 7], 11);
+    ll sum = version_number(&bits[idx]);
+    idx += 18;
+    FOR(i, num_subpackets) {
+        sum += parse_packet(bits, idx);
+    }
+    return sum;
+}
+
+ll parse_literal(vi &bits, int &idx) {
+    cerr << "parsing literal" << endl;
+    ll sum = version_number(&bits[idx]);
+    idx += 6;
+    do {
+        idx++;
+        // (number)
+        idx += 4;
+    } while (bits[idx - 5] == 1);
+
+    return sum;
+}
+
+ll parse_packet(vi &bits, int &idx) {
+    ll type = packet_type(&bits[idx]);
+    if (type == 4) {
+        return parse_literal(bits, idx);
+    } else {
+        ll len_type = length_type(&bits[idx]);
+        if (len_type == 0)
+            return parse_lt0(bits, idx);
+            // return 0;
+        else
+            return parse_lt1(bits, idx);
+    }
+
+}
+
+ll sum_version_nums(vi &bits) {
+    int idx = 0;
+    ll sum = 0;
+    sum += parse_packet(bits, idx);
+    // while (idx < bits.size()) {
+    //     sum += parse_packet(bits, idx);
+    // }
+    return sum;
+}
+
+void solve() {
+    string input;
+    cin >> input;
+    vi bits(input.size() * 4);
+    FOR(i, input.size()) {
+        int a;
+        if (input[i] >= '0' && input[i] <= '9') {
+            a = input[i] - '0';
+        } else {
+            a = input[i] - 'A' + 10;
+        }
+        bits[4 * i + 3] = (a >> 0) & 1;
+        bits[4 * i + 2] = (a >> 1) & 1;
+        bits[4 * i + 1] = (a >> 2) & 1;
+        bits[4 * i + 0] = (a >> 3) & 1;
+    }
+    debug(bits);
+    debug(sum_version_nums(bits));
+}
+
